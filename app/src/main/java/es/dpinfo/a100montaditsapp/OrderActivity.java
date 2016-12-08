6969985db8f126bc978e21f928446319d76480a3
David <dprimenko@protonmail.com>
2016-12-08 23:57:44 +0100

    Initial commit

---
+++ app/src/main/java/es/dpinfo/a100montaditsapp/OrderActivity.java
@@ -0,0 +1,16 @@
+package es.dpinfo.a100montaditsapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dprimenko on 8/12/16.
+ */
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
